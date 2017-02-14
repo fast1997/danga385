0ccd7bf9db0dc305af7a47c4d95b07a1fc37e914
Jeffrey Fisher <jeff@bam-043374-jf.local>
2017-02-14 15:57:38 -0800

    added imports

--- Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
+++ Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jeff.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
